45c3cf35468db584372bc09a9532a7ecc51e01d0
ngt-kn <nagatak@Dev.local>
2018-05-12 21:44:54 -0700

    adds save to ListTableViewController, segue complete

--- MyListApp/Controller/ListTableViewController.swift
+++ MyListApp/Controller/ListTableViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 import RealmSwift
 
 class ListTableViewController: UITableViewController {
-    let realm = try! Realm()
-    var listItem : Results<ListItem>?
     
+    let realm = try! Realm()  // initialize realm
+    var listItem : Results<ListItem>?  // declare a new object
+    
+    // set up for segue
     var selectedCategory : Category? {
         didSet {
             loadItems()
@@ -21,28 +23,71 @@ class ListTableViewController: UITableViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadItems()
-
     }
 
 
 
     // MARK: - Table view data source
 
-
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // if count != nil, return count. Else return 1.
         return listItem?.count ?? 1
     }
 
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
-
+        // if itemName != nil add itemName to list. else list is empty
         cell.textLabel?.text = listItem?[indexPath.row].itemName ?? "Please add an item"
 
         return cell
     }
 
+    //Mark: - delegate methods
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    //MARK: Add items
+    @IBAction func addItemButton(_ sender: UIBarButtonItem) {
+        var nameTextField = UITextField()
+        
+        let alert = UIAlertController(title: "Add Item", message: "", preferredStyle: .alert)
+        
+        let action = UIAlertAction(title: "Add", style: .default) { (action) in
+            // if selectedCategory != nil, create a new object and append
+            if let currentCategory = self.selectedCategory {
+                do {
+                    try self.realm.write{
+                        let newItem = ListItem()
+                        newItem.itemName = nameTextField.text!
+                        currentCategory.items.append(newItem)
+                    }
+                } catch {
+                    print("Error saving item, \(error)")
+                }
+            }
+            // Reload the table
+            self.tableView.reloadData()
+        }
+        
+        
+        // Add text field for itemName
+        alert.addTextField { (name) in
+            name.placeholder = "Enter Item Name"
+            nameTextField = name
+        }
+        
+        alert.addAction(action)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    
+    //MARK: - data manipulation methods
+    
+    // Load objects from realm
     func loadItems() {
         listItem = selectedCategory?.items.sorted(byKeyPath: "itemName", ascending: true)
         
